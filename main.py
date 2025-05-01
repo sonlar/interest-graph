3b4c986373be5551f01601d16c111431af0948e7
sonlar <31799749+sonlar@users.noreply.github.com>
2025-05-01 18:21:46 +0200

    get stargazers

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from dotenv import load_dotenv
 from github import Auth, Github
 from os import getenv
+import networkx
 
 
 class Github_API:
 
     def __init__(self, token, user, repo) -> None:
-        self.auth = Auth.Token(token)
+        self.auth= Auth.Token(token)
         self.user = user
         self.repo = repo
 
-    def authorize(self) -> None:
-        g = Github(auth=self.auth)
-        print(g.get_user(self.user).get_repo(self.repo))
-        g.close()
+    def start(self) -> None:
+        self.session = Github(auth=self.auth, per_page=100)
+        self.get_stargazers()
+
+    def stop(self) -> None:
+        self.session.close()
+
+    def get_stargazers(self):
+        stargazers = [stargazer for stargazer in self.session.get_user(self.user).get_repo(self.repo).get_stargazers()]
 
 
 if "__main__" == __name__:
@@ -21,6 +27,7 @@ if "__main__" == __name__:
     token = getenv("token")
     user = "ptwobrussell"
     repo = "Mining-the-Social-Web"
-    CRAWL_DA_WEB = Github_API(token, user, repo)
-    CRAWL_DA_WEB.authorize()
+    CRAWL_DA_SOCIAL_WEB = Github_API(token, user, repo)
+    CRAWL_DA_SOCIAL_WEB.start()
+    CRAWL_DA_SOCIAL_WEB.stop()
 
